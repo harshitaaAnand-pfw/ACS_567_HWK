d05a5cb5c8061aceba0febbb5b0a0d12ca135ccc
harshitaaAnand-pfw <122410158+harshitaaAnand-pfw@users.noreply.github.com>
2023-01-28 21:17:18 -0500

    Hwk2 (#5)
    * initial commit
    
    * initial commit
    
    * Update UserPrefs.xml
    
    * Heart disease Rest Apis

---
+++ HeartDisease/HeartDisease/Controllers/HeartDiseaseAnalysisController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeartDisease.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HeartDiseaseAnalysisController : ControllerBase
+    {
+        private readonly ILogger<HeartDiseaseAnalysisController> _logger;
+
+        public HeartDiseaseAnalysisController(ILogger<HeartDiseaseAnalysisController> logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        /// get all the data
+        /// </summary>
+        /// <returns> all data</returns>
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<HeartDiseaseAnalysis>))]
+        public IActionResult GetHeartDiseaseAnalysis()
+        {
+            return Ok(HeartDiseaseAnalysisRepository.getInstance().getHearts());
+        }
+
+
+        /// <summary>
+        /// get data by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>data by id</returns>
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(HeartDiseaseAnalysis))]
+        [ProducesResponseType(404)]
+
+        public IActionResult GetHeartDisease(int id)
+        {
+            HeartDiseaseAnalysis heart = HeartDiseaseAnalysisRepository.getInstance().GetHeart(id);
+
+
+            if (heart == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(heart);
+            }
+
+        }
+        /// <summary>
+        /// Add a data
+        /// </summary>
+        /// <param name="heart"></param>
+        /// <returns>new data</returns>
+
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+
+        public IActionResult CreateAnalysis([FromBody] HeartDiseaseAnalysis heart)
+        {
+            if (heart == null)
+            {
+                return BadRequest("Heart is null");
+            }
+
+            bool result = HeartDiseaseAnalysisRepository.getInstance().addHeart(heart);
+
+            if (result)
+            {
+                return Ok("Successfully added");
+            }
+            else
+            {
+                return BadRequest("Heart not added");
+            }
+        }
+
+
+        /// <summary>
+        /// Update with respct to id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="heart"></param>
+        /// <returns>updated value</returns>
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+
+        public IActionResult UpdateHeart(int id, HeartDiseaseAnalysis heart)
+        {
+            if (heart == null)
+            {
+                return BadRequest("heart is null");
+            }
+            bool isUpdated = HeartDiseaseAnalysisRepository.getInstance().editHeart(id, heart);
+
+            if (!isUpdated)
+            {
+                return NotFound("No matching heart");
+            }
+            else
+            {
+                return Ok("Successfully updated");
+            }
+        }
+
+        /// <summary>
+        /// delete by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>deleted</returns>
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteHeart(int id)
+        {
+            bool deleted = HeartDiseaseAnalysisRepository.getInstance().deleteHeart(id);
+
+            if (!deleted)
+            {
+                return NotFound("No matching id");
+            }
+            else
+            {
+                return Ok("Heart deleted");
+            }
+        }
+
+        /// <summary>
+        /// analyse the data
+        /// </summary>
+        /// <returns> all data</returns>
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(string))]
+        [Route("age")]
+        public IActionResult GetHeartDiseaseDataAnalysis(int age)
+        {
+            return Ok(HeartDiseaseAnalysisRepository.getInstance().getHeartsAnalysis(age));
+        }
+    }
+}
+
---
+++ HeartDisease/HeartDisease/Repository/HeartDiseaseAnalysisRepository.cs
@@ -0,0 +1,158 @@
+﻿using System;
+namespace HeartDisease
+{
+    public class HeartDiseaseAnalysisRepository
+    {
+        private static HeartDiseaseAnalysisRepository instance;
+        private List<HeartDiseaseAnalysis> hearts;
+
+        private HeartDiseaseAnalysisRepository()
+        {
+            hearts = new();
+            hearts.Add(new HeartDiseaseAnalysis("add data"));
+        }
+
+        public static HeartDiseaseAnalysisRepository getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new();
+            }
+            return instance;
+        }
+        /// <summary>
+        /// get all the data
+        /// </summary>
+        /// <returns>get all data</returns>
+        public List<HeartDiseaseAnalysis> getHearts()
+        {
+            return hearts;
+        }
+        /// <summary>
+        /// get the data by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>data by id</returns>
+        public HeartDiseaseAnalysis GetHeart(int id)
+        {
+            HeartDiseaseAnalysis heart = null;
+
+            foreach (HeartDiseaseAnalysis h in hearts)
+            {
+                if (id == h.Id)
+                {
+                    heart = h;
+                    break;
+
+                }
+            }
+            return heart;
+        }
+        /// <summary>
+        /// add the data
+        /// </summary>
+        /// <param name="heart"></param>
+        /// <returns>new value</returns>
+        public bool addHeart(HeartDiseaseAnalysis heart)
+        {
+            bool isAdded = true;
+
+            foreach (HeartDiseaseAnalysis h in hearts)
+            {
+                if (h.Id == heart.Id)
+                {
+                    isAdded = false;
+                    break;
+                }
+            }
+            if (isAdded)
+            {
+                hearts.Add(heart);
+            }
+            return isAdded;
+        }
+        /// <summary>
+        /// update the data
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="updated"></param>
+        /// <returns>updated value</returns>
+        public bool editHeart(int id, HeartDiseaseAnalysis updated)
+        {
+            bool isEdited = false;
+
+            foreach (HeartDiseaseAnalysis h in hearts)
+            {
+                if (h.Id == id)
+                {
+                    h.Description = updated.Description;
+                    h.IsCompleted = updated.IsCompleted;
+                    isEdited = true;
+                    break;
+
+                }
+            }
+
+            return isEdited;
+        }
+        /// <summary>
+        /// delete the data
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>null</returns>
+        public bool deleteHeart(int id)
+        {
+            HeartDiseaseAnalysis delete = null;
+
+            foreach (HeartDiseaseAnalysis h in hearts)
+            {
+                if (id == h.Id)
+                {
+                    delete = h;
+                    break;
+                }
+            }
+
+            if (delete != null)
+            {
+                hearts.Remove(delete);
+            }
+
+            return delete == null;
+        }
+
+        /// <summary>
+        /// analysis of the data
+        /// </summary>
+        /// <returns>analyse all data</returns>
+        public string getHeartsAnalysis(int age)
+
+        {
+
+            if (age >= 0 && age <= 18)
+            {
+              //  Console.WriteLine("You are very less likely prone to a heart disease, still stay active and healthy");
+                return "You are very less likely prone to a heart disease, still stay active and healthy";
+
+
+            }
+            else if (age > 18 && age <= 45)
+            {
+              //  Console.WriteLine("You may be prone to heart disease if you have a unhealthy lifestyle, Please take regular medical checkups to prevent it ");
+                return "You may be prone to heart disease if you have a unhealthy lifestyle, Please take regular medical checkups to prevent it ";
+
+
+            }
+            else
+            {
+             //   Console.WriteLine("You are more likeley prone to heart disease, have regular health checkups and have a healthy lifestyle ");
+                return "You are more likeley prone to heart disease, have regular health checkups and have a healthy lifestyle ";
+
+
+            }
+            
+            
+        }
+    }
+}
+
